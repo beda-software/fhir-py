9efefd96acdb6f8527345e68855608a15801b907
ruscoder <rusmiligamer@gmail.com>
2018-06-18 16:39:53 +0700

    Add structure for pypi package

---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import re
+from io import open
+
+from setuptools import setup
+
+
+try:
+    from pypandoc import convert
+
+    def read_md(f):
+        return convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found,"
+          " could not convert Markdown to RST")
+
+    def read_md(f):
+        return open(f, 'r', encoding='utf-8').read()
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+version = get_version('aidbox')
+
+
+setup(
+    name='aidbox',
+    version=version,
+    url='http://github.com/beda-software/aidbox-py',
+    license='BSD',
+    description='Aidbox client for python',
+    long_description=read_md('README.md'),
+    keywords='aidbox',
+    author='beda.software',
+    author_email='aidbox@beda.software',
+    packages=['aidbox'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
