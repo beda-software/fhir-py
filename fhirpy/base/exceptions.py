0a33d2fc424a8c2cd804ef46cedf938831abd6bd
Benedikt Vogler <developer@benediktsvogler.com>
2021-06-17 14:28:59 +0200

    only pass error message without HTML

--- fhirpy/base/exceptions.py
+++ fhirpy/base/exceptions.py
@@ -1,3 +1,5 @@
+import json
+
 class BaseFHIRError(Exception):
     pass
 
@@ -16,13 +18,8 @@ class AuthorizationError(BaseFHIRError):
 
 class OperationOutcome(BaseFHIRError):
     def __init__(self, *args, **kwargs):
-        text: str = args[0]["text"]
-        text = text.removeprefix(
-            "<div xmlns=\"http://www.w3.org/1999/xhtml\"><h1>Operation Outcome</h1><table border=\"0\"><tr><td style=\"font-weight: bold;\">ERROR</td><td>[]</td><td><pre>").removesuffix(
-            "</pre></td>\n\t\t\t</tr>\n\t\t</table>\n\t</div>")
-        error = {"text": text,
-                 "issue": args[0]["issue"]}
-        super(args, kwargs)
+        error = json.loads(args[0])["issue"]
+        super().__init__(error, kwargs)
 
 
 class MultipleResourcesFound(BaseFHIRError):
