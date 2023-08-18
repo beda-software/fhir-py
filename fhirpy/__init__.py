17389c46ece2a4f816556f58888155e0443b7f8a
Ilya Beda <ir4y.ix@gmail.com>
2023-08-18 17:23:04 +1000

    Update year

--- fhirpy/__init__.py
+++ fhirpy/__init__.py
@@ -4,7 +4,7 @@ __title__ = "fhir-py"
 __version__ = "1.3.2"
 __author__ = "beda.software"
 __license__ = "None"
-__copyright__ = "Copyright 2022 beda.software"
+__copyright__ = "Copyright 2023 beda.software"
 
 # Version synonym
 VERSION = __version__
