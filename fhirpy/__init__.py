230b8e209e624c35e00dc6912d3d8ee6f64d7e47
Ilya Beda <ir4y.ix@gmail.com>
2023-08-18 17:23:14 +1000

    Bump 1.4.1

--- fhirpy/__init__.py
+++ fhirpy/__init__.py
@@ -1,7 +1,7 @@
 from .lib import SyncFHIRClient, AsyncFHIRClient
 
 __title__ = "fhir-py"
-__version__ = "1.3.2"
+__version__ = "1.4.1"
 __author__ = "beda.software"
 __license__ = "None"
 __copyright__ = "Copyright 2023 beda.software"
