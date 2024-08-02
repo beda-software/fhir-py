9b1829d8a7960c5135613a8b53ea9102b298711a
Vadim Laletin <rusmiligamer@gmail.com>
2024-08-02 13:54:38 +0200

    Add ruff, mypy, autohooks

--- fhirpy/__init__.py
+++ fhirpy/__init__.py
@@ -4,7 +4,7 @@ __title__ = "fhir-py"
 __version__ = "1.4.2"
 __author__ = "beda.software"
 __license__ = "None"
-__copyright__ = "Copyright 2023 beda.software"
+__copyright__ = "Copyright 2024 beda.software"
 
 # Version synonym
 VERSION = __version__
